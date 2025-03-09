6323a1dbb6756207092cb5e09d58991723d5d0ed
EderJoel26 <eder_1321124031@uptecamac.edu.mx>
2025-03-09 02:21:04 -0600

    feat: solving problems to compile

--- seyfert.config.mjs
+++ seyfert.config.mjs
@@ -1,14 +1,15 @@
 import { config } from "seyfert";
+import "dotenv/config";
 
 export default config.bot({
-    token: process.env.BOT_TOKEN ?? "",
-    locations: {
-        base: "dist", // replace with "src" if using bun
-        commands: "commands"
-    },
-    intents: ["Guilds"],
-    // This configuration is optional, in case you want to receive interactions via HTTP
-    // This allows you to use both the gateway and the HTTP webhook
-    publicKey: "...", // replace with your public key
-    port: 4444, // replace with your application's port
+  token: process.env.BOT_TOKEN ?? "",
+  locations: {
+    base: "dist", // replace with "src" if using bun
+    commands: "commands",
+    middlewares: "middlewares",
+    events: "events",
+  },
+  intents: ["Guilds"],
+  publicKey: process.env.PUBLIC_KEY,
+  port: process.env.PORT ?? 3000,
 });
--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,30 +1,23 @@
-import {
-    type CommandContext,
-    Middlewares,
-    LocalesT,
-    Declare,
-    Command,
-  } from 'seyfert';
-  
-  @Declare({
-    name: 'ping',
-    description: 'Show latency with Discord',
-    props: {
-      ratelimit: {
-        type: 'user',
-        time: 5_000,
-      },
+import { type CommandContext, Middlewares, Declare, Command } from "seyfert";
+
+@Declare({
+  name: "ping",
+  description: "Show latency with Discord",
+  props: {
+    ratelimit: {
+      type: "user",
+      time: 5_000,
     },
-  })
-  @LocalesT('commands.ping.name', 'commands.ping.description')
-  @Middlewares(['Cooldown',])
-  export default class PingCommand extends Command {
-    async run(ctx: CommandContext) {
-      await ctx.deferReply();
-      const ping = ctx.client.gateway.latency;
-  
-      await ctx.editOrReply({
-        content: ctx.t.commands.ping.res({ ping, }).get(),
-      });
-    }
+  },
+})
+@Middlewares(["Cooldown"])
+export default class PingCommand extends Command {
+  async run(ctx: CommandContext) {
+    await ctx.deferReply();
+    const ping = ctx.client.gateway.latency;
+
+    await ctx.editOrReply({
+      content: "Pong! Latency: **" + ping + "ms**",
+    });
   }
+}
